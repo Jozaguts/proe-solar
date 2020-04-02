deea949909249deeabc6463c8e8d1f10b8eb86c5
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-04-02 12:33:37 -0600

    remove whatsApp icon from the navbar

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="container max-w-full bg-info-color">
-     <div class="container mx-auto grid md:grid-cols-3 grid-cols-3">
+     <div class="container mx-auto grid md:grid-cols-3 grid-cols-2 ">
        <div class="flex flex-col m-5">
          <h1 class="text-white font-bold  mb-3">Nosotros</h1>
          <ul>
@@ -18,7 +18,7 @@
             <li class=" text-main-dark-color font-medium mb-3">Calentadores Solares</li>
           </ul>
         </div>
-       <div class="flex flex-col m-5">
+       <div class="xl:flex lg:flex md:flex hidden flex-col m-5">
           <h1 class="text-white font-bold  mb-3">Contacto</h1>
           <div class="grid grid-cols-3">
             <a href="https://www.facebook.com/PROE-SOLAR-104413387841116" target="_blank">
@@ -33,13 +33,35 @@
           </div>
          <div class="flex mt-5">
               <span class="icon icon-call-us"> </span>
-           <div class="flex flex-col">
-             <em class="ml-3">322-312-312-31</em>
-             <em class="ml-3">322-312-312-31</em>
-           </div>
+<!--           <div class="flex flex-col">-->
+             <a href="tel:3221029376" class="ml-4"><em class="text-main-dark-color">322-102-9376</em></a>
+             <a href="tel:3221699397" class="ml-4"><em class="text-main-dark-color">322-169-9397</em></a>
+<!--           </div>-->
          </div>
         </div>
      </div>
+<!--    only on mobile-->
+    <div class="xl:hidden lg:hidden md:hidden flex flex-col m-5">
+      <h1 class="text-white font-bold  mb-3">Contacto</h1>
+      <div class="grid grid-cols-3">
+        <a href="https://www.facebook.com/PROE-SOLAR-104413387841116" target="_blank">
+          <span class="icon icon-facebook m-auto"></span>
+        </a>
+        <a href="">
+          <span class="icon icon-whatsapp m-auto"></span>
+        </a>
+        <a href="mailto:solarproe@gmail.com">
+          <span class="icon icon-email m-auto"></span>
+        </a>
+      </div>
+      <div class="flex mt-5">
+        <span class="icon icon-call-us mx-auto"> </span>
+        <div class="flex flex-col mx-auto">
+          <a href="tel:3221029376"><em class="text-main-dark-color">322-102-9376</em></a>
+          <a href="tel:3221699397"><em class="text-main-dark-color">322-169-9397</em></a>
+        </div>
+      </div>
+    </div>
     <div class="foot container mx-auto text-main-dark-color font-bold">
       <h2> &copy; {{date}} ProeSolar.com</h2>
     </div>
--- components/ServicesArea.vue
+++ components/ServicesArea.vue
@@ -5,7 +5,7 @@
       <h1 class="font-bold text-2xl md:text-5xl mb-2 capitalize"> Nuestros servicios</h1>
       <h2 class="text-center text-gray-700 mb-8"> Lorem ipsum dolor sit amet, consectetur adipisicing </h2>
   </div>
-  <div class="grid col-gap-4 row-gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 md:text-2xl lg:grid-cols-2  xl:grid-cols-4 services-card-container ">
+  <div class="grid col-gap-4 row-gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 md:text-2xl lg:grid-cols-2  xl:grid-cols-2 services-card-container ">
     <div class="services-card-container__service" v-for="servicio in servicios"  :key="servicio.id">
       <service-card  :data="servicio"> </service-card>
     </div>
@@ -48,31 +48,31 @@
             ],
 
           },
-          {
-            id: 3,
-            src: "/img/heater.svg",
-            alt: "imagen calentadores solares",
-            title: "Calentadores Solares",
-            features:[
-              'Venta de equipo.',
-              'Instalación (tubería y conexión del equipo).',
-              'Mantenimiento',
-
-            ],
-
-          },
-          {
-            id: 4,
-            src: "/img/air-conditioner.svg",
-            alt: "imagen calentadores solares",
-            title: "Aires Acondicionados",
-            features:[
-              'Venta de equipo.',
-              'Instalación',
-              'Mantenimiento'
-            ],
-
-          },
+          // {
+          //   id: 3,
+          //   src: "/img/heater.svg",
+          //   alt: "imagen calentadores solares",
+          //   title: "Calentadores Solares",
+          //   features:[
+          //     'Venta de equipo.',
+          //     'Instalación (tubería y conexión del equipo).',
+          //     'Mantenimiento',
+          //
+          //   ],
+          //
+          // },
+          // {
+          //   id: 4,
+          //   src: "/img/air-conditioner.svg",
+          //   alt: "imagen calentadores solares",
+          //   title: "Aires Acondicionados",
+          //   features:[
+          //     'Venta de equipo.',
+          //     'Instalación',
+          //     'Mantenimiento'
+          //   ],
+          //
+          // },
         ]
       }
     },
